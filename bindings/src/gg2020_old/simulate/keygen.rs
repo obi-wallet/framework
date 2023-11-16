edb10fd4d00faec0212f05a032cd5bc78c9b5dbe
Luca Spinazzola <mspinluca@gmail.com>
2023-11-16 03:00:05 -0500

    fix keygen_simulated

--- bindings/src/gg2020_old/simulate/keygen.rs
+++ bindings/src/gg2020_old/simulate/keygen.rs
@@ -1,9 +1,11 @@
 //! Key generation bindings simulated
 use wasm_bindgen::{JsError, JsValue};
 use wasm_bindgen::prelude::wasm_bindgen;
+use mpc_driver::gg2020_old::simulate::keygen::keygen_simulated_impl;
 
 /// Bindings to simulation based key generation bindings
 #[wasm_bindgen(js_name = "keygenSimulated")]
 pub fn keygen_simulated(parameters: JsValue) -> Result<JsValue, JsError> {
-    Ok(serde_wasm_bindgen::to_value(&serde_wasm_bindgen::from_value(parameters)?)?)
+    let keyshares = keygen_simulated_impl(serde_wasm_bindgen::from_value(parameters)?)?;
+    Ok(serde_wasm_bindgen::to_value(&keyshares)?)
 }
--- driver/src/gg2020_old/simulate/keygen.rs
+++ driver/src/gg2020_old/simulate/keygen.rs
@@ -9,7 +9,7 @@ use crate::gg20::KeyShare;
 use crate::gg_2020::state_machine::keygen::Keygen;
 
 /// Generate keys in simulation mode
-pub fn keygen_simulated(parameters: Parameters) -> Result<Vec<KeyShare>, Error> {
+pub fn keygen_simulated_impl(parameters: Parameters) -> Result<Vec<KeyShare>, Error> {
     let t = parameters.threshold;
     let n = parameters.parties;
     let mut simulation = Simulation::<Keygen>::new();
