66b36195c8768da07605bc744e817daf722bfcee
Luca Spinazzola <mspinluca@gmail.com>
2023-11-21 22:06:24 -0500

    try single threaded tokio

--- bindings/src/gg2020_old/simulate/keygen.rs
+++ bindings/src/gg2020_old/simulate/keygen.rs
@@ -5,7 +5,13 @@ use mpc_driver::gg2020_old::simulate::keygen::keygen_simulated_impl;
 
 /// Bindings to simulation based key generation bindings
 #[wasm_bindgen(js_name = "keygenSimulated")]
-pub async fn keygen_simulated(parameters: JsValue) -> Result<JsValue, JsError> {
-    let keyshares = keygen_simulated_impl(serde_wasm_bindgen::from_value(parameters)?).await?;
+pub fn keygen_simulated(parameters: JsValue) -> Result<JsValue, JsError> {
+    let rt = tokio::runtime::Builder::new_current_thread()
+        .enable_all()
+        .build()
+        .unwrap();
+    let keyshares = rt.block_on(async {
+        keygen_simulated_impl(serde_wasm_bindgen::from_value(parameters)?).await?
+    });
     Ok(serde_wasm_bindgen::to_value(&keyshares)?)
 }
