ea181043244fe5d2b785c0373cc8051ffd298c0e
Luca Spinazzola <mspinluca@gmail.com>
2023-11-21 21:56:17 -0500

    try using AsyncSimulation for keygen_simulated

--- bindings/src/gg2020_old/simulate/keygen.rs
+++ bindings/src/gg2020_old/simulate/keygen.rs
@@ -5,7 +5,7 @@ use mpc_driver::gg2020_old::simulate::keygen::keygen_simulated_impl;
 
 /// Bindings to simulation based key generation bindings
 #[wasm_bindgen(js_name = "keygenSimulated")]
-pub fn keygen_simulated(parameters: JsValue) -> Result<JsValue, JsError> {
-    let keyshares = keygen_simulated_impl(serde_wasm_bindgen::from_value(parameters)?)?;
+pub async fn keygen_simulated(parameters: JsValue) -> Result<JsValue, JsError> {
+    let keyshares = keygen_simulated_impl(serde_wasm_bindgen::from_value(parameters)?).await?;
     Ok(serde_wasm_bindgen::to_value(&keyshares)?)
 }
--- driver/src/gg2020_old/simulate/keygen.rs
+++ driver/src/gg2020_old/simulate/keygen.rs
@@ -1,24 +1,24 @@
 //! Key generation simulated
 
 use cggmp_threshold_ecdsa::mpc_ecdsa::gg_2020::state_machine::sign::Error;
+use round_based::dev::AsyncSimulation;
 
 use mpc_protocol::Parameters;
-use crate::gg2020_old::simulate::simulation::Simulation;
 
 use crate::gg20::KeyShare;
 use crate::gg_2020::state_machine::keygen::Keygen;
 
 /// Generate keys in simulation mode
-pub fn keygen_simulated_impl(parameters: Parameters) -> Result<Vec<KeyShare>, Error> {
+pub async fn keygen_simulated_impl(parameters: Parameters) -> Result<Vec<KeyShare>, Error> {
     let t = parameters.threshold;
     let n = parameters.parties;
-    let mut simulation = Simulation::<Keygen>::new();
+    let mut simulation = AsyncSimulation::<Keygen>::new();
 
     for i in 1..=n {
         simulation.add_party(Keygen::new(i, t, n).unwrap());
     }
 
-    let keys = simulation.run().unwrap();
+    let keys: Vec<KeyShare> = simulation.run().await.into_iter().map(|k| k.unwrap()).collect();
 
     Ok(keys.into_iter().map(|k| k.into()).collect())
 }
