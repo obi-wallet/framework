1637ee4d3ae194bf56a8b48e735e9589164c8172
Luca Spinazzola <mspinluca@gmail.com>
2023-11-27 12:09:50 -0500

    reimplement distributed key refresh

--- driver/src/cggmp_old/refresh.rs
+++ driver/src/cggmp_old/refresh.rs
@@ -1,81 +1,67 @@
-// use std::collections::HashMap;
-//
-// use cggmp_threshold_ecdsa::refresh;
-// use cggmp_threshold_ecdsa::refresh::state_machine;
-// use curv::elliptic::curves::Secp256k1;
-// use round_based::{Msg, StateMachine};
-// use wasm_bindgen::prelude::*;
-//
-// use mpc_protocol::Parameters;
-//
-// use crate::gg_2020::state_machine::keygen::LocalKey;
-// use crate::round::RoundMsg;
-//
-// /// Key refresh.
-// #[wasm_bindgen]
-// pub struct KeyRefresh {
-//     inner: refresh::state_machine::KeyRefresh,
-// }
-//
-// #[wasm_bindgen]
-// impl KeyRefresh {
-//     /// Create a key refresh.
-//     #[wasm_bindgen(constructor)]
-//     pub fn new(
-//         parameters: JsValue,
-//         local_key: JsValue,
-//         new_party_index: JsValue,
-//         old_to_new: JsValue,
-//         current_t: JsValue,
-//     ) -> Result<KeyRefresh, JsError> {
-//         let params: Parameters = serde_wasm_bindgen::from_value(parameters)?;
-//         let local_key: Option<LocalKey<Secp256k1>> =
-//             serde_wasm_bindgen::from_value(local_key)?;
-//         let new_party_index: Option<u16> =
-//             serde_wasm_bindgen::from_value(new_party_index)?;
-//         let old_to_new: HashMap<u16, u16> =
-//             serde_wasm_bindgen::from_value(old_to_new)?;
-//         let current_t_option: Option<u16> =
-//             serde_wasm_bindgen::from_value(current_t)?;
-//
-//         Ok(Self {
-//             inner: state_machine::KeyRefresh::new(
-//                 local_key,
-//                 new_party_index,
-//                 &old_to_new,
-//                 params.threshold,
-//                 params.parties,
-//                 current_t_option,
-//             )?,
-//         })
-//     }
-//
-//     /// Handle an incoming message.
-//     #[wasm_bindgen(js_name = "handleIncoming")]
-//     pub fn handle_incoming(
-//         &mut self,
-//         message: JsValue,
-//     ) -> Result<(), JsError> {
-//         let message: Msg<
-//             <state_machine::KeyRefresh as StateMachine>::MessageBody,
-//         > = serde_wasm_bindgen::from_value(message)?;
-//         self.inner.handle_incoming(message)?;
-//         Ok(())
-//     }
-//
-//     /// Proceed to the next round.
-//     pub fn proceed(&mut self) -> Result<JsValue, JsError> {
-//         self.inner.proceed()?;
-//         let messages = self.inner.message_queue().drain(..).collect();
-//         let round = self.inner.current_round();
-//         let messages = RoundMsg::from_round(round, messages);
-//         Ok(serde_wasm_bindgen::to_value(&(round, &messages))?)
-//     }
-//
-//     /// Get the key share.
-//     pub fn create(&mut self) -> Result<JsValue, JsError> {
-//         let local_key = self.inner.pick_output().unwrap()?;
-//         let key_share: KeyShare = local_key.into();
-//         Ok(serde_wasm_bindgen::to_value(&key_share)?)
-//     }
-// }
+//! Key refresh
+use std::collections::HashMap;
+
+use cggmp_threshold_ecdsa::refresh::state_machine;
+use cggmp_threshold_ecdsa::refresh::state_machine::{
+    Error, ProtocolMessage,
+};
+use curv::elliptic::curves::Secp256k1;
+use round_based::{Msg, StateMachine};
+
+use mpc_protocol::Parameters;
+
+use crate::gg20::KeyShare;
+use crate::gg_2020::state_machine::keygen::LocalKey;
+use crate::round::RoundMsg;
+
+/// Key refresh.
+pub struct KeyRefreshWrapper(state_machine::KeyRefresh);
+
+impl KeyRefreshWrapper {
+    /// Create a key refresh.
+    pub fn new(
+        parameters: Parameters,
+        local_key: Option<LocalKey<Secp256k1>>,
+        new_party_index: Option<u16>,
+        old_to_new: HashMap<u16, u16>,
+        current_t: Option<u16>,
+    ) -> Result<KeyRefreshWrapper, Error> {
+        Ok(KeyRefreshWrapper(state_machine::KeyRefresh::new(
+            local_key,
+            new_party_index,
+            &old_to_new,
+            parameters.threshold,
+            parameters.parties,
+            current_t,
+        )?))
+    }
+
+    /// Handle an incoming message.
+    pub fn handle_incoming(
+        &mut self,
+        message: Msg<
+            <state_machine::KeyRefresh as StateMachine>::MessageBody,
+        >,
+    ) -> Result<(), Error> {
+        self.0.handle_incoming(message)?;
+        Ok(())
+    }
+
+    /// Proceed to the next round.
+    pub fn proceed(
+        &mut self,
+    ) -> Result<(u16, Vec<RoundMsg<ProtocolMessage>>), Error> {
+        self.0.proceed()?;
+        let messages = self.0.message_queue().drain(..).collect();
+        let round = self.0.current_round();
+        let messages = RoundMsg::from_round(round, messages);
+        Ok((round, messages))
+    }
+
+    /// Get the key share.
+    pub fn create(&mut self) -> Result<KeyShare, Error> {
+        let local_key = self.0.pick_output().unwrap()?;
+        let key_share: KeyShare = local_key.into();
+        Ok(key_share)
+    }
+}
